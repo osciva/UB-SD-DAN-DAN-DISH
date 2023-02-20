e068c11b4809944f9a83b564d78eb22494096a2e
oscar <oscardecaraltroy@gmail.com>
2023-02-20 11:00:09 +0100

    test: Refactoring implemented

--- Client/src/GameClient.java
+++ Client/src/GameClient.java
@@ -1,7 +1,30 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
 public class GameClient {
 
     /*
     TO DO.
     Class that encapsulates the game's logic. Sequence of states following the established protocol .
      */
+
+    public void play(Socket socket, String message) {
+
+        try {
+            DataOutputStream data_stream = new DataOutputStream(socket.getOutputStream());
+            data_stream.writeUTF(message);
+            data_stream.flush();
+            data_stream.close();
+        } catch (IOException e) {
+            throw new RuntimeException("I/O Error when creating or sending the output stream. Is the host connected?:\n"+e.getMessage());
+        }
+
+        // Add game execution logic here
+    }
+
+
+
 }
--- Server/src/GameHandler.java
+++ Server/src/GameHandler.java
@@ -1,8 +1,43 @@
-public class GameHandler {
-
-    /*
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+/*
     TO DO
     Protocol dynamics from Server.
     Methods: run(), init(), play().
-     */
+*/
+
+public class GameHandler extends Thread {
+    private Socket socket;
+
+    public GameHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+
+    public void init(){
+
+    }
+
+    @Override
+    public void run() {
+
+        String message = null;
+        try {
+            DataInputStream data_input = new DataInputStream(socket.getInputStream());
+            message = data_input.readUTF();
+            System.out.println("The client send the following message:\n" + message);
+
+            data_input.close();
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("I/O Error when reading the client's message:\n" + e.getMessage());
+        }
+
+
+    }
+
+    public void play(){
+
+    }
 }
