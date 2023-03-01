89223b8aff8bcb1c7e841e1fd4117b88b40ff7fe
ivanmansilla <ivanmansillaflores@gmail.com>
2023-03-01 21:43:12 +0100

    Proceso de Send y Recived Hello

--- Client/src/ClientProtocol.java
+++ Client/src/ClientProtocol.java
@@ -14,14 +14,18 @@ public class ClientProtocol {
             DataOutputStream data_outPut = new DataOutputStream(socket.getOutputStream());
             byte[] buffer = {0, 0};
             data_outPut.writeByte(opCode); // Capçalera, serà un 1, perque es el HELLO
-            data_outPut.writeChars(name); // Longitud sense limit //FALLA A PARTIR DE  AQUI Y NO SE PORQUE AUN CREO PORQUE EN EL DATA OTUPUT SE DEBE ENVIAR COMO TOODO EN UNA LISTA O ALGO PERO SOLO PERMITE ESCRIBIR UNA COSA
-            //data_outPut.flush();
-            data_outPut.write(buffer); // Indica el final de trama
-            //data_outPut.flush();
             data_outPut.writeInt(id);
 
+            data_outPut.writeChars(name); // Longitud sense limit //FALLA A PARTIR DE  AQUI Y NO SE PORQUE AUN CREO PORQUE EN EL DATA OTUPUT SE DEBE ENVIAR COMO TOODO EN UNA LISTA O ALGO PERO SOLO PERMITE ESCRIBIR UNA COSA
+
+            //data_outPut.flush();
+            data_outPut.write(0); // Indica el final de trama
+            data_outPut.write(0); // Indica el final de trama
+            //data_outPut.flush();
+
+
             data_outPut.flush();
-            data_outPut.close();
+            // data_outPut.close();
         } catch (IOException e) {
             throw new RuntimeException("I/O Error when creating or sending the output stream. Is the host connected?:\n" + e.getMessage());
         }
