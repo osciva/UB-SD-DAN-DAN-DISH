d0c4634b6a706f072f5c05318ae48caa40372fe8
ivanmansilla <ivanmansillaflores@gmail.com>
2023-03-15 16:35:57 +0100

    sendResult complete!!

--- Server/src/GameProtocol.java
+++ Server/src/GameProtocol.java
@@ -5,9 +5,10 @@ import java.net.Socket;
 
 public class GameProtocol {
 
-
     private Socket socket;
     private int id;
+    private String accioRebuda;
+    private int contBales;
     private DataOutputStream data_outPut;
     private DataInputStream data_inPut;
     private util utils;
@@ -57,15 +58,15 @@ public class GameProtocol {
         try (DataOutputStream data_outPut = new DataOutputStream(socket.getOutputStream())) {
             // HACER UN SWITCH CASE, Y DEPENDE DEL ERROR QUE SEA, ENVIARA UN MENSAJE
             // DETERMINADO QUE LO PODEMOS PONER AQUI O SINO NADA ESTA BIEN ASI
-            utils.escriureByte( opCode); // OpCode a tornar
-            utils.escriureByte( error); // Indica el eror que ha fet
+            utils.escriureByte(opCode); // OpCode a tornar
+            utils.escriureByte(error); // Indica el eror que ha fet
             for (int i = 0; i < msg.length(); i++) {
                 char p = msg.charAt(i);
-                utils.escriureChar( p);
+                utils.escriureChar(p);
             }
             utils.escriureChar('0');// Indica el final de trama
-            byte[] array = {0, 0};
-            utils.writeBytes( array);
+            byte[] array = { 0, 0 };
+            utils.writeBytes(array);
             data_outPut.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
@@ -115,10 +116,10 @@ public class GameProtocol {
     public void sendAdmit(Socket socket) {
         try {
             byte opCode = 4;
-            utils.escriureByte( opCode);
+            utils.escriureByte(opCode);
             System.out.println("Enviem opCode to Admit: \n" + opCode);
             int isAdmit = 1; // 1 si admitim, 0 si no (potser si hi ha un error enviem 0)
-            utils.escriureInt( isAdmit);
+            utils.escriureInt(isAdmit);
             System.out.println("Enviem int to Admit: \n" + isAdmit);
             utils.ferFlush();
 
@@ -127,22 +128,6 @@ public class GameProtocol {
 
         }
     }
-
-
-    public void sendResult(Socket socket) {
-        try {
-            byte opCode = 6;
-            utils.escriureByte( opCode);
-            System.out.println("Enviem opCode to result: \n" + opCode);
-
-
-        } catch (IOException msg) {
-            throw new RuntimeException("error a sentResult");
-
-        }
-    }
-
-
 
     public boolean receivedAction(Socket socket) {
         try {
@@ -157,6 +142,7 @@ public class GameProtocol {
                 System.out.println("The client sent the following opCode and wants to play:\n" + opCode);
                 this.id = utils.llegirInt();
                 String accio = utils.llegirAction();
+                this.accioRebuda = accio;
                 System.out.println("C -----ACTION " + accio + " -----> S");
                 return true;
             }
@@ -167,7 +153,79 @@ public class GameProtocol {
         return false;
     }
 
+    public void sendResult(Socket socket) {
+        try {
+            byte opCode = 6;
+            utils.escriureByte(opCode);
+            System.out.println("Enviem opCode to result: \n" + opCode);
+            int random = 0;
+            if (contBales > 0) {
+                random = (int) (Math.random() * 3 + 1);
+            } else {
+                random = (int) (Math.random() * 3 + 2);
+            }
+            String accioServer = "";
+            String result = "";
+            String accioClient = this.accioRebuda.toUpperCase();
+            switch (random) {
+                case 1:
+                    accioServer = "SHOOT";
+                    contBales -= 1;
+                    if (accioClient.equals("SHOOT")) {
+                        result = "DRAW0"; // Client i Servidor disparen, empat
+                        utils.escriureString(result);
+                        break;
+                    } else if (accioClient.equals("CHARGE")) {
+                        result = "ENDS0"; // Client recarrega, Servidor dispara i guanya
+                        utils.escriureString(result);
+                        break;
+                    } else {
+                        result = "SAFE1"; // Client bloqueja, Servidor dispara, bloqueig del client
+                        utils.escriureString(result);
+                        break;
+                    }
 
+                case 2:
+                    accioServer = "BLOCK";
+                    if (accioClient.equals("SHOOT")) {
+                        result = "SAFE0"; // Client dispara, Servidor bloqueja.
+                        utils.escriureString(result);
+                        break;
+                    } else if (accioClient.equals("CHARGE")) {
+                        result = "PLUS1"; // Client recarrega una bala perque servidor bloqueja
+                        utils.escriureString(result);
+                        break;
+                    } else {
+                        result = "SAFE2"; // Client i Servidor bloquejen els dos
+                        utils.escriureString(result);
+                        break;
+                    }
+                case 3:
+                    accioServer = "CHARGE";
+                    contBales += 1;
+                    if (accioRebuda.toUpperCase().equals("SHOOT")) {
+                        result = "ENDS1"; // Client dispara, client guanya
+                        utils.escriureString(result);
+                        break;
+                    } else if (accioRebuda.toUpperCase().equals("CHARGE")) {
+                        result = "PLUS2"; // Client i Servidor recarreguen una bala
+                        utils.escriureString(result);
+                        break;
+
+                    } else {
+                        result = "PLUS0"; // Client bloqueja, Servidor recarrega una bala
+                        utils.escriureString(result);
+                        break;
+                    }
+                default:
+                    System.out.println("Ha habido un error");
+            }
+
+        } catch (IOException msg) {
+            throw new RuntimeException("error a result");
+
+        }
+    }
 
     public boolean receivedError(Socket socket) {
         return false;
